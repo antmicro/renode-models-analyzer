310ee170a0f2495ba008b978591bdc2fd432f88e
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2024-10-21 12:46:01 +0200

    [#65857] installModelsCompare: Allow installing validate feature

--- Scripts/installModelsCompare.sh
+++ Scripts/installModelsCompare.sh
@@ -2,6 +2,35 @@
 
 # install ModelsCompare as a Python package, with runnable script
 
+features=()
+while getopts ":v-:" opt; do
+    case $opt in
+        v)
+            features+=("validate")
+            ;;
+        -)
+            case "$OPTARG" in
+                validate)
+                    features+=("validate")
+                    ;;
+                *)
+                    echo "Invalid option: --$OPTARG" >&2
+                    exit 1
+                    ;;
+            esac
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+feature_string=$(IFS=, ; echo "${features[*]}")
+if [ -n "$feature_string" ]; then
+    feature_string="[$feature_string]"
+fi
+
 cd ModelsCompare/
 rm -r build dist *.egg-info src/*.egg-info 2> /dev/null || true
-pip3 install --user .
+pip3 install --user ".$feature_string"
