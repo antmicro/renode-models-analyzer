a89f062074f4b410d6dc5398e5ee07dce5207255
Mateusz Karlic <mkarlic@antmicro.com>
2025-02-11 09:12:56 +0100

    [#72610] Optional user installation

--- Scripts/installModelsCompare.sh
+++ Scripts/installModelsCompare.sh
@@ -3,15 +3,22 @@
 # install ModelsCompare as a Python package, with runnable script
 
 features=()
+options=()
 while getopts ":v-:" opt; do
     case $opt in
         v)
             features+=("validate")
             ;;
+        u)
+            options+=("--user")
+            ;;
         -)
             case "$OPTARG" in
                 validate)
                     features+=("validate")
+                    ;;
+                user)
+                    options+=("--user")
                     ;;
                 *)
                     echo "Invalid option: --$OPTARG" >&2
@@ -33,4 +40,8 @@ fi
 
 cd ModelsCompare/
 rm -r build dist *.egg-info src/*.egg-info 2> /dev/null || true
-pip3 install --user ".$feature_string"
+if [ -n "$options" ]; then
+    pip3 install "${options[*]}" ".$feature_string"
+else
+    pip3 install ".$feature_string"
+fi
